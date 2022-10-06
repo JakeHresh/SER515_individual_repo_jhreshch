fcebcc2cf6b09185069cd0dc3563a807efe7ccdc
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2022-10-05 22:12:43 -0700

    Added Dummy Code with Design Pattern Structures

---
+++ SER515DesignPatterns/ClassProductList.java
@@ -0,0 +1,15 @@
+public class ClassProductList extends ArrayList {
+
+	private ReminderVisitor reminderVisitor;
+
+	private ProductIterator productIterator;
+
+	private ProductIterator productIterator;
+
+	private Product[] product;
+
+	public void accept(NodeVisitor visitor) {
+
+	}
+
+}
---
+++ SER515DesignPatterns/OfferingIterator.java
@@ -0,0 +1,21 @@
+public class OfferingIterator implements ListIterator {
+
+	private OfferingList offeringList;
+
+	public boolean hasNext() {
+		return false;
+	}
+
+	public void Next() {
+
+	}
+
+	public void MoveToHead() {
+
+	}
+
+	public void Remove() {
+
+	}
+
+}

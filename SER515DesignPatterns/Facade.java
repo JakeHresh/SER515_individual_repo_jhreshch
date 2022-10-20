b8525710e25100f3065a31e420748adf715a2998
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2022-10-20 09:34:02 -0700

    Added Facade comments and initialized Facade in main

--- SER515DesignPatterns/Facade.java
+++ SER515DesignPatterns/Facade.java
@@ -1,59 +1,93 @@
 public class Facade {
 
-	private int UserType;
+	private int UserType; // buyer = 0, seller = 1
 
-	private Product theSelectedProduct;
+	private Product theSelectedProduct; // The object that holds the currently selected product.
 
-	private int nProductCategory;
+	private int nProductCategory; //The selected product category: 0: Meat, 1: Produce.
 
-	private ClassProductList theProductList;
+	private ClassProductList theProductList;//The list of products of the entire system.
 
-	private Person thePerson;
+	private Person thePerson;//The current user.
 
+	public Facade(int userType, Product selectedProduct, int productCategory, ClassProductList classProductList, Person person)
+	{
+		UserType = userType;
+		theSelectedProduct = selectedProduct;
+		nProductCategory = productCategory;
+		theProductList = classProductList;
+		thePerson = person;
+	}
+	//Show login GUI and return the login result.
 	public boolean login() {
 		return false;
 	}
-
+	/*
+	* When clicking the add button of the ProductMenu, call this
+	function. This function will add a new trade and fill in the
+	required information. This function will be called
+	SellerTradingMenu or BuyerTradingMenu based on the type of
+	the user. It will not update the product menu. The product menu
+	needs to be refreshed outside the function.*/
 	public void addTrading() {
 
 	}
-
+	/*
+	* When clicking the view button of the ProductMenu, call this
+	function and pass the pointer of the Trading and the person
+	pointer to this function.
+	This function will view the trading information.
+	This function will call SellerTradingMenu or BuyerTradingMenu
+	according to the type of the user.*/
 	public void viewTrading() {
 
 	}
-
+	/*
+	* This function will view the given offering.*/
 	public void decideBidding() {
 
 	}
-
+	//Set the deal flag of the given offering.
 	public void discussBidding() {
 
 	}
-
+	//Used by the buyer to submit the offering.
 	public void submitBidding() {
 
 	}
-
+	/*
+	* Show the remind box to remind buyer of the upcoming overdue
+	trading window.*/
 	public void remind() {
 
 	}
-
+	/*
+	* Create a user object according to the userinfoitem, the object
+	can be a buyer or a seller*/
 	public void createUser(UserInfoItem userinfoitem) {
 
 	}
-
+	//Create the product list of the entire system.
 	public void createProductList() {
 
 	}
-
+	/*
+	* Call this function after creating the user. Create productList by
+	reading the UserProduct.txt file. Match the product name with
+	theProductList. Attach the matched product object to the new
+	create user: Facade.thePerson. ProductList*/
 	public void AttachProductToUser() {
 
 	}
-
+	//Show the Product list in a Dialog and return the selected product.
 	public Product SelectProduct() {
 		return null;
 	}
-
+	/*
+	* This function will call the thePerson. CreateProductMenu0
+	According to the real object (buyer or seller) and the
+	productLevel, it will call different menu creator and show the
+	menu differently according to the usertype.*/
 	public void productOperation() {
 
 	}
--- SER515DesignPatterns/Main.java
+++ SER515DesignPatterns/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello!");
+        Facade facade = new Facade(0, null, 0, null, null);
     }
 }
